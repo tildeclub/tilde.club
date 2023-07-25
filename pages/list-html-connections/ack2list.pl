e3964f5eb66fe202fe7d3823ffdd38d5280bdb98
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2023-07-24 23:43:51 -0600

    Update ack2list.pl
    fixed errors

--- pages/list-html-connections/ack2list.pl
+++ pages/list-html-connections/ack2list.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
-my %h = {};
+my %h;
 while (<STDIN>) {
     chomp;
     my ($file, $line, $tilde) = split /:/;
@@ -8,8 +8,8 @@ while (<STDIN>) {
     $file =~ s/\/home\//\/~/;
     $file =~ s/public_html\///;
     $h{$tilde}{$file}++;
-
 }
+
 print qq{<html><head><title>Tilde.Club Connections</title>
 <link rel="stylesheet" type="text/css" href="css/social.css">
 </head>
@@ -19,14 +19,15 @@ print qq{<html><head><title>Tilde.Club Connections</title>
 
 print "<table>";
 for my $x (sort keys %h) {
-    print "<tr><td class=\"user\"><a href=\"/$x\">$x</td><td class=\"linkedby\">is linked by</td><td class=\"linkers\">";
+    print "<tr><td class=\"user\"><a href=\"/$x\">$x</a></td><td class=\"linkedby\">is linked by</td><td class=\"linkers\">";
     for my $y (sort keys %{$h{$x}}) {
-	my $user = $y;
-	$user =~ m/(\~\w+)/;
-	print "<a name=\"$user\" href=\"$user\">$1</a> ";
-
+        my $user = $y;
+        if ($user =~ /~(\w+)/) {
+            my $username = "~$1"; # Add the tilde (~) before the username
+            print "<a href=\"/$username\">$username</a> ";
+        }
     }
-    print "</td></tr>\n"
-};
+    print "</td></tr>\n";
+}
 print "</table>";
 print qq{</body></html>};
